a44e109a503ef12032f50628fd327a1738dc5930
Vicky Andre <v_key13@hotmail.com>
2020-02-19 06:28:49 -0500

    task manager 1 through 7

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(safe_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task = Task.update(safe_params)
+  end
+
+  private
+
+  def safe_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
